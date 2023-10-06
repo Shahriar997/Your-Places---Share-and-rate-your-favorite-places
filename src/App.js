bf0dea1ac4f659f3ab6d7b39e265e3cd982e0309
Shahriar Hossain <shahriar.hossain997@gmail.com>
2023-10-07 01:35:35 +0600

    added token expiration in the local storage

--- src/App.js
+++ src/App.js
@@ -18,10 +18,18 @@ const App = () => {
   const [token, setToken] = useState(false);
   const [userId, setUserId] = useState(false);
 
-  const login = useCallback((uid, token) => {
+  const login = useCallback((uid, token, expirationDate) => {
     setToken(token);
     setUserId(uid);
-    localStorage.setItem('userData', JSON.stringify({userId: uid, token: token}))
+    const tokenExpirationDate = expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uid,
+        token: token,
+        expiration: tokenExpirationDate.toISOString(),
+      })
+    );
   }, []);
 
   const logout = useCallback(() => {
@@ -32,8 +40,12 @@ const App = () => {
 
   useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem('userData'));
-    if (storedData && storedData.token) {
-      login(storedData.userId, storedData.token);
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId, storedData.token, storedData.expirationDate);
     }
   }, [login]);
 
